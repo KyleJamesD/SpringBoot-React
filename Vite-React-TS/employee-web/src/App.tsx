93ac89ea0c4c2c5deee0403765b689230e2cc490
kyledyck@outlook.com <144828203+KyleJamesD@users.noreply.github.com>
2025-03-30 19:32:14 -0600

    Finished Add employee

--- Vite-React-TS/employee-web/src/App.tsx
+++ Vite-React-TS/employee-web/src/App.tsx
@@ -1,15 +1,17 @@
 'use client'
 import { useState, useEffect } from 'react'
 import './App.css'
-import { Person } from './assets/schema'
 import axios from 'axios'
+import { Person } from './schema'
+
+
 
 function App() {
 
   const [employees,setEmployees] = useState<Person[]>([]);
   const [name, setName] = useState<string>(); 
   const [manager, setManager] = useState<string>();
-  const [salary, setSalary] = useState<number>();
+  const [salary, setSalary] = useState<string>();
 
   useEffect(() => {
     getAllEmployees();
@@ -32,7 +34,47 @@ function App() {
       }))
     }
 
+    const handleNameChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+      setName(e.target.value);
+    };
 
+    const handleManagerChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+      setManager(e.target.value);
+    };
+
+    const handleSalaryChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+      setSalary(e.target.value);
+    };
+
+    const handleNewEmpSubmit = async () => {
+      if(!name || !manager || !salary){
+        alert("Please Fill in All Fields!");
+        return;
+      }
+      if(typeof salary != "string"){
+        alert("Salary must be a Number!!");
+        return;
+      }
+        const newemp : Person = {
+          name: name,
+          manager: manager,
+          salary: Number(salary),
+        }
+        try{
+          const response = await axios.post("http://localhost:8085/employees", newemp)
+          console.log(response);
+          handleCancel();
+          getAllEmployees();
+         }catch(error){
+           console.log(error);
+         }
+    }
+
+    function handleCancel () {
+      setName("");
+      setManager("");
+      setSalary("");
+    }
   return (
     <>
       <div className='main-container'>
@@ -40,18 +82,18 @@ function App() {
         <div className='add-panel'>
           <div className='addpaneldiv'>
             <label htmlFor='name'>Name</label><br></br>
-            <input className='addpanelinput' value={name} type='text' name="name" id="name"></input>
+            <input className='addpanelinput' value={name} onChange={handleNameChange} type='text' name="name" id="name"></input>
           </div>
           <div className='addpaneldiv'>
             <label htmlFor='manager'>Manager</label><br></br>
-            <input className='addpanelinput' value={manager} type='text' name="manager" id="manager"></input>
+            <input className='addpanelinput' value={manager} onChange={handleManagerChange} type='text' name="manager" id="manager"></input>
           </div>
           <div className='addpaneldiv'>
             <label htmlFor='salary'>Salary</label><br></br>
-            <input className='addpanelinput' value={salary}  type='text' name="salary" id="salary"></input>
+            <input className='addpanelinput' value={salary} onChange={handleSalaryChange} type='text' name="salary" id="salary"></input>
           </div>
-          <button className='addbtn' > Add</button>
-          <button className='cancelbtn' >Cancel</button>
+          <button className='addbtn' onClick={handleNewEmpSubmit}> Add</button>
+          <button className='cancelbtn' onClick={handleCancel}>Cancel</button>
         </div>
         <input className='searchinput' type='search' name='inputsearch' id='inputsearch' placeholder='Search Employee by Name'></input>
       </div>
@@ -66,14 +108,13 @@ function App() {
         </tr>
       </thead>
       <tbody>
-        
+        {rows}
       </tbody>
     </table>
     </div>
 
     </>
   )
-
 }
 
 export default App;
